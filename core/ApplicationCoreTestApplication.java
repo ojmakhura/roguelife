63f715fbe431d4943e2851b2534a6ac93b5cfb74
ojmakhura <ojmakhura@systemsjr.com>
2023-10-05 18:26:28 +0200

    Add files via upload

---
+++ core/ApplicationCoreTestApplication.java
@@ -0,0 +1,7 @@
+package bw.co.roguesystems.life;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguelifeCoreTestApplication {
+    
+}
